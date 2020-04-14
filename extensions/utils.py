6abe3b63ad4508ceeb450ff72e9fe90ba03a77b3
Matthew lowe <jedijasper2004@gmail.com>
2020-04-14 13:41:16 +0100

    Can't remember but VS Code says it changed

--- extensions/utils.py
+++ extensions/utils.py
@@ -11,10 +11,11 @@ class Utils(commands.Cog):
 	# Adds embed footer, timestamp + color, used to have all embeds look the same, ctx = command context
 	# TODO: Allow for context to be None if no arg given, allows for function to be used outside command. Will add when I need it
 	def style_embed(self, embed, ctx):
+		embed.timestamp = datetime.datetime.utcnow()
+		
 		member = ctx.message.guild.get_member(ctx.author.id)
 		embed.set_footer(text=f"Requested by {member.display_name}#{ctx.author.discriminator}",
 						 icon_url=ctx.author.avatar_url_as(format='png'))
-		embed.timestamp = datetime.datetime.utcnow()
 		roles = member.roles
 		color = roles[len(roles)-1].color
 		embed.color = color
