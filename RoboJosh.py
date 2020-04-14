b349e6ea917d7a24149e2583edd1fc3896ab16e2
Matthew lowe <jedijasper2004@gmail.com>
2020-04-14 13:40:43 +0100

    Added extension management commands

--- RoboJosh.py
+++ RoboJosh.py
@@ -23,4 +23,25 @@ async def on_ready():
 	print("Loaded extensions!")
 
 
+# Manage extensions from a command
+# TODO: check with database to ensure random users can't unload extensions lol
+@bot.command()
+async def extensions(ctx, method, extension):
+	try:
+		if method == "load":
+			bot.load_extension('extensions.' + extension)
+			await ctx.send(f"Extension '{extension}' loaded!")
+		elif method == "unload":
+			bot.unload_extension('extensions.' + extension)
+			await ctx.send(f"Extension '{extension}' unloaded!")
+		elif method == "reload":
+			bot.reload_extension('extensions.' + extension)
+			await ctx.send(f"Extension '{extension}' reloaded!")
+		else:
+			await ctx.send("Unrecognised extension command!")
+	except commands.errors.ExtensionNotFound:
+		await ctx.send("Error caught (ExtensionNotFound)")
+	except commands.errors.ExtensionNotLoaded:
+		await ctx.send("Error caught (ExtensionNotLoaded)")
+
 bot.run(open("token.txt", "r").read())
